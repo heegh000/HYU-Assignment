910ac5e136110d392273c95b2f93f80a3649147a
2018009234 <dewpe000@naver.com>
2019-05-03 23:21:34 +0900

    0503BST

--- 0503BST/BST.c
+++ 0503BST/BST.c
@@ -3,9 +3,9 @@
 
 typedef struct Node
 {
-	int data;
-	struct Node* leftChild;
-	struct Node* rightChild;
+   int data;
+   struct Node* leftChild;
+   struct Node* rightChild;
 }Node;
 
 void insertTreeNode(Node** p, int value);
@@ -21,205 +21,205 @@ void copyTreeNode(Node* src, Node** dst);
 int compareTwoTree(Node* t1, Node* t2);
 
 Node* searchTreeNode(Node* p, int value) {
-	while(p->data != value) {
-	
-		if(p->data < value)
-			p = p->rightChild;
-		else if(p->data > value)
-			p = p->leftChild;
+   while(p->data != value) {
+   
+      if(p->data < value)
+         p = p->rightChild;
+      else if(p->data > value)
+         p = p->leftChild;
 
-	}
+   }
 
-	return p;
+   return p;
 }
 
 Node* searchTreeParentNode(Node* p, int value) {
-	Node* parentNode = NULL;
+   Node* parentNode = NULL;
 
-	while(p->data != value) {
-		
-		parentNode = p;
+   while(p->data != value) {
+      
+      parentNode = p;
 
-		if(p->data < value)
-			p = p->rightNode;
-		else if(p->data > value)
-			p = p->leftNode;
-	}
+      if(p->data < value)
+         p = p->rightChild;
+      else if(p->data > value)
+         p = p->leftChild;
+   }
 
-	return parentNode;
+   return parentNode;
 
 }
 
 void deleteTreeNode(Node** p, int value) {
 
-	Node* parentNode;
-	Node* delNode;
-	Node* childNode;
-	Node* succNode;
+   Node* parentNode;
+   Node* delNode;
+   Node* childNode;
+   Node* succNode;
 
-	parentNode = searchTreeParentNode(*p, value);
-	delNode = searchTreeNode(*p, value);
+   parentNode = searchTreeParentNode(*p, value);
+   delNode = searchTreeNode(*p, value);
 
-	if(delNode == NULL) {
-		printf("There is no %d\n", value);
-		return;
-	}
+   if(delNode == NULL) {
+      printf("There is no %d\n", value);
+      return;
+   }
 
-	if(delNode->leftChild == NULL && delNode->rightChild == NULL) {
-		if(parentNode == NULL) 
-			*p = NULL:
+   if(delNode->leftChild == NULL && delNode->rightChild == NULL) {
+      if(parentNode == NULL) 
+         *p = NULL;
 
-		else {
-			if(parentNode->leftNode == delNode) 
-				parentNode->leftNode = NULL;
-			else
-				parentNode->rightNode = NULL;
-		}
-	}
-	else if(delNode->leftChild == NULL || delNode->rightNode == NULL) {
-		childNode = (delNode->leftNode != NULL) ? delNode->leftChild : delNode->rightChild;
-		
-		if(parentNode == NULL) 
-			*p = NULL;
-		else {
-			if(parentNode->leftChild == delNode)
-				parentNode->leftNode = childNode;
-			else
-				parentNode->rightNode = childNode;
-		}
-	}
-	
+      else {
+         if(parentNode->leftChild == delNode) 
+            parentNode->leftChild = NULL;
+         else
+            parentNode->rightChild = NULL;
+      }
+   }
+   else if(delNode->leftChild == NULL || delNode->rightChild == NULL) {
+      childNode = (delNode->leftChild != NULL) ? delNode->leftChild : delNode->rightChild;
+      
+      if(parentNode == NULL) 
+         *p = NULL;
+      else {
+         if(parentNode->leftChild == delNode)
+            parentNode->leftChild = childNode;
+         else
+            parentNode->rightChild = childNode;
+      }
+   }
+   
 }
 
 
 int main()
 {
-	Node* pParentNode = NULL;
-	Node* cParentNode = NULL;
+   Node* pParentNode = NULL;
+   Node* cParentNode = NULL;
 
-	///Insert Data into Tree(pParentNode)
-	insertTreeNode(&pParentNode, 4);
-	insertTreeNode(&pParentNode, 2);
-	insertTreeNode(&pParentNode, 6);
-	insertTreeNode(&pParentNode, 1);
-	insertTreeNode(&pParentNode, 3);
-	insertTreeNode(&pParentNode, 5);
-	insertTreeNode(&pParentNode, 7);
+   ///Insert Data into Tree(pParentNode)
+   insertTreeNode(&pParentNode, 4);
+   insertTreeNode(&pParentNode, 2);
+   insertTreeNode(&pParentNode, 6);
+   insertTreeNode(&pParentNode, 1);
+   insertTreeNode(&pParentNode, 3);
+   insertTreeNode(&pParentNode, 5);
+   insertTreeNode(&pParentNode, 7);
 
-	///Print Data in Tree(pParentNode)
-	printf("Tree(pParentNode)\n");
-	printf("Preorder\t:");
-	printTreePreorder(pParentNode);
-	printf("\nInorder\t\t:");
-	printTreeInorder(pParentNode);
-	printf("\nPostorder\t:");
-	printTreePostorder(pParentNode);
-	printf("\n\n");
+   ///Print Data in Tree(pParentNode)
+   printf("Tree(pParentNode)\n");
+   printf("Preorder\t:");
+   printTreePreorder(pParentNode);
+   printf("\nInorder\t\t:");
+   printTreeInorder(pParentNode);
+   printf("\nPostorder\t:");
+   printTreePostorder(pParentNode);
+   printf("\n\n");
 
-	///Search Data in Tree(pParentNode) 
-	printf("Search Data 3\n");
-	if (searchTreeNode(pParentNode, 3) != NULL)
-		printf("3 is existed\n\n");
-	else
-		printf("3 is not existed\n\n");
+   ///Search Data in Tree(pParentNode) 
+   printf("Search Data 3\n");
+   if (searchTreeNode(pParentNode, 3) != NULL)
+      printf("3 is existed\n\n");
+   else
+      printf("3 is not existed\n\n");
 
-	///Delete Data in Tree(pParentNode)
-	//printf("Delete data 3\n");
-	//deleteTreeNode(&pParentNode, 3);
-	//printf("Delete data 2\n");
-	//deleteTreeNode(&pParentNode, 2);
-	printf("Delete data 6\n");
-	deleteTreeNode(&pParentNode, 6);
+   ///Delete Data in Tree(pParentNode)
+   //printf("Delete data 3\n");
+   //deleteTreeNode(&pParentNode, 3);
+   //printf("Delete data 2\n");
+   //deleteTreeNode(&pParentNode, 2);
+   printf("Delete data 6\n");
+   deleteTreeNode(&pParentNode, 6);
 
-	///Search Data in Tree(pParentNode) 
-	printf("Search Data 4\n");
-	if (searchTreeNode(pParentNode, 6) != NULL)
-		printf("6 is existed\n\n");
-	else
-		printf("6 is not existed\n\n");
+   ///Search Data in Tree(pParentNode) 
+   printf("Search Data 4\n");
+   if (searchTreeNode(pParentNode, 6) != NULL)
+      printf("6 is existed\n\n");
+   else
+      printf("6 is not existed\n\n");
 
-	///Print Data in Tree(pParentNode)
-	printf("Tree(pParentNode)\n");
-	printf("Preorder\t:");
-	printTreePreorder(pParentNode);
-	printf("\nInorder\t\t:");
-	printTreeInorder(pParentNode);
-	printf("\nPostorder\t:");
-	printTreePostorder(pParentNode);
-	printf("\n\n");
+   ///Print Data in Tree(pParentNode)
+   printf("Tree(pParentNode)\n");
+   printf("Preorder\t:");
+   printTreePreorder(pParentNode);
+   printf("\nInorder\t\t:");
+   printTreeInorder(pParentNode);
+   printf("\nPostorder\t:");
+   printTreePostorder(pParentNode);
+   printf("\n\n");
 
-	///Copy Tree(pParentNode) into Tree(cParentNode)
-	copyTreeNode(pParentNode, &cParentNode);
+   ///Copy Tree(pParentNode) into Tree(cParentNode)
+   copyTreeNode(pParentNode, &cParentNode);
 
-	///Print Data in Tree(cParentNode)
-	printf("Preorder\t:");
-	printTreePreorder(cParentNode);
-	printf("\nInorder\t\t:");
-	printTreeInorder(cParentNode);
-	printf("\nPostorder\t:");
-	printTreePostorder(cParentNode);
-	printf("\n\n");
+   ///Print Data in Tree(cParentNode)
+   printf("Preorder\t:");
+   printTreePreorder(cParentNode);
+   printf("\nInorder\t\t:");
+   printTreeInorder(cParentNode);
+   printf("\nPostorder\t:");
+   printTreePostorder(cParentNode);
+   printf("\n\n");
 
-	///Compare Data between two Trees
-	if (compareTwoTree(pParentNode, cParentNode))
-		printf("Two trees are equal\n");
-	else
-		printf("Two trees are different\n");
+   ///Compare Data between two Trees
+   if (compareTwoTree(pParentNode, cParentNode))
+      printf("Two trees are equal\n");
+   else
+      printf("Two trees are different\n");
 
 
-	return 0;
+   return 0;
 }
 
 
 void insertTreeNode(Node** p, int value)
 {
-	if ((*p) == NULL)
-	{
-		// Create TreeNode with value
-		(*p) = (Node*)malloc(sizeof(Node));
-		(*p)->data = value;
-		(*p)->leftChild = NULL;
-		(*p)->rightChild = NULL;
-	}
-	else if ((*p)->data > value)
-	{
-		// Recursive call to leftChild
-		insertTreeNode(&((*p)->leftChild), value);
-	}
-	else
-	{
-		// Recursive call to rightChild
-		insertTreeNode(&((*p)->rightChild), value);
-	}
+   if ((*p) == NULL)
+   {
+      // Create TreeNode with value
+      (*p) = (Node*)malloc(sizeof(Node));
+      (*p)->data = value;
+      (*p)->leftChild = NULL;
+      (*p)->rightChild = NULL;
+   }
+   else if ((*p)->data > value)
+   {
+      // Recursive call to leftChild
+      insertTreeNode(&((*p)->leftChild), value);
+   }
+   else
+   {
+      // Recursive call to rightChild
+      insertTreeNode(&((*p)->rightChild), value);
+   }
 }
 
 
 void printTreePreorder(Node* pNode)
 {
-	if (pNode == NULL)
-		return;
+   if (pNode == NULL)
+      return;
 
-	printf("%3d", pNode->data);
-	printTreePreorder(pNode->leftChild);
-	printTreePreorder(pNode->rightChild);
+   printf("%3d", pNode->data);
+   printTreePreorder(pNode->leftChild);
+   printTreePreorder(pNode->rightChild);
 }
 
 void printTreeInorder(Node* pNode)
 {
-	if (pNode == NULL)
-		return;
+   if (pNode == NULL)
+      return;
 
-	printTreeInorder(pNode->leftChild);
-	printf("%3d", pNode->data);
-	printTreeInorder(pNode->rightChild);
+   printTreeInorder(pNode->leftChild);
+   printf("%3d", pNode->data);
+   printTreeInorder(pNode->rightChild);
 }
 
 void printTreePostorder(Node* pNode)
 {
-	if (pNode == NULL)
-		return;
+   if (pNode == NULL)
+      return;
 
-	printTreePostorder(pNode->leftChild);
-	printTreePostorder(pNode->rightChild);
-	printf("%3d", pNode->data);
+   printTreePostorder(pNode->leftChild);
+   printTreePostorder(pNode->rightChild);
+   printf("%3d", pNode->data);
 }
