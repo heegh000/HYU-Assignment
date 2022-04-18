9d0d898c0e730adc04b5b9482437cfe89fe49887
heegh000 <dewpe000@naver.com>
2022-04-18 11:50:08 +0000

    Update test.c

--- xv6-public/test.c
+++ xv6-public/test.c
@@ -9,23 +9,23 @@ int
 main(int argc, char *argv[]) 
 {
 
-	int pid = fork();
+  int pid = fork();
 
-	if(pid > 0) {
-		set_cpu_share(10);
-		wait();
-	}
-	else if (pid == 0) {
-		set_cpu_share(0);
-		sleep(200);
-		printf(1, "change\n");
-		set_cpu_share(10);
-		sleep(200);
-		exit();
-	}
+  if(pid > 0) {
+    set_cpu_share(10);
+    wait();
+  }
+  else if (pid == 0) {
+    set_cpu_share(0);
+    sleep(200);
+    printf(1, "change\n");
+    set_cpu_share(10);
+    sleep(200);
+    exit();
+  }
 
-	printf(1, "done\n");
-	sleep(200);
+  printf(1, "done\n");
+  sleep(200);
 
-	exit();
+  exit();
 }
