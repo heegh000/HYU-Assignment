2a0abf84b17b36bcf4f638a82e1ebb29d591cd29
heegh000 <dewpe000@naver.com>
2022-04-18 11:48:40 +0000

    Update proc.h

--- xv6-public/proc.h
+++ xv6-public/proc.h
@@ -41,10 +41,10 @@ struct proc {
   char *kstack;                // Bottom of kernel stack for this process
   enum procstate state;        // Process state
   int pid;                     // Process ID
-	int level;									 // Process Level 
-	int idx; 										 // Index of ptalbe.proc
-	uint ticks;									 // How long the process runs
-	int tickets;								 // CPU resources of the process
+  int level;                   // Process Level 
+  int idx;                     // Index of ptalbe.proc
+  uint ticks;                  // How long the process runs
+  int tickets;                 // CPU resources of the process
   struct proc *parent;         // Parent process
   struct trapframe *tf;        // Trap frame for current syscall
   struct context *context;     // swtch() here to run process
